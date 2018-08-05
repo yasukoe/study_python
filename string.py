636b222cfddcdef28b15c8391400fbc266a40f1a
yasukoe <mail@hifiwidow.com>
2018-08-05 23:56:02 +0100

    added comments with description

--- string.py
+++ string.py
@@ -1,22 +1,48 @@
 ''' this file is for studing 'strings' '''
 print('Hello HiFiWidow')
+# using variable
+msg = 'Hello HiFiwidow'
+print(msg)
 
-test_str = 'Hello HiFiwidow'
-print(test_str)
-
-test_str = """
+# using comment for inserting strings
+msg = """
 test_1
 test_2
 """
-print(test_str)
+print(msg)
 
-test_str = 'Python'
-test_str += "-"
-test_str += "commit"
-print(test_str)
+#using += to add more values
+msg = 'Python'
+msg += "-"
+msg += "commit"
+print(msg)
 
-test_str = '012'*3
-print(test_str)
+#increment the values -> 012012012
+msg = '012'*3
+print(msg)
 
+#change the number to strings
 int = 100
 print(str(int) + 'yen')
+
+#replace the values
+msg = 'HiFiWidow'
+print(msg.replace('HiFi','Python'))
+
+#aplit the strings
+msg = 'HiFi-Widow'
+print(msg.split('-'))
+
+#right-justified with filling the specified spaces
+msg = '1234'
+print(msg.rjust(10,'0'))
+print(msg.rjust(10,'!'))
+
+#filling by zero
+print(msg.zfill(10))
+print(msg.zfill(3))
+
+#start from startswith
+msg = 'LearningPython'
+print(msg.startswith('Python'))
+print(msg.startswith('Learning'))
