8ed130fb46aaacc090cadf215413fbccc43a8d17
yasukoe <mail@hifiwidow.com>
2018-08-05 19:05:24 +0100

    3rd trial

--- string.py
+++ string.py
@@ -9,3 +9,14 @@ test_1
 test_2
 """
 print(test_str)
+
+test_str = 'Python'
+test_str += "-"
+test_str += "commit"
+print(test_str)
+
+test_str = '012'*3
+print(test_str)
+
+int = 100
+print(str(int) + 'yen')
