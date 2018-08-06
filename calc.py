59dcaa84abef41e8f22aca5512909517ee99433d
yasukoe <mail@hifiwidow.com>
2018-08-06 13:05:28 +0100

    calculation

---
+++ calc.py
@@ -0,0 +1,19 @@
+# + - * / // % **
+x = 10
+print(x/3) #3.333333
+print(x//3) #3
+print(x%3) #1
+print(x**2) #100
+
+y = 4
+y += 12
+print(y) #16
+
+# and or # NOT
+print(True and False) #False
+print(True or False) #True
+print(not True) #False
+
+# + *
+print('hello ' + 'world') #hello world
+print('hello ' * 3) #hello hello hello
